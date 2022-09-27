0779025745bfb51e2e34b2e4791998d89704a111
DanielAtwood <atwood1999@gmail.com>
2022-09-27 02:53:52 -0500

    Updated Navigation bar Created CSS to hide bars for responsive Added search bar Added Items for Checkout

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,9 +3,11 @@ import React from 'react';
 const NavBar = () => {
     return (
         <div className="header">
+            {/*HTML for logo/image*/}
             <a href="#" className="logo">
                 <img src="/coffee.jpg" alt="logo"/>
             </a>
+            {/*HTML for content links*/}
             <nav className={"nav-content"}>
                 <a href={"#home"}>Home</a>
                 <a href={"#about"}>About</a>
@@ -15,13 +17,61 @@ const NavBar = () => {
                 <a href={"#contact"}>Contact</a>
                 <a href={"#blogs"}>Blogs</a>
             </nav>
+
+            {/*HTML for buttons*/}
             <div className="icons">
-            <div className="fas fa-search" id="search-btn"></div>
-            <div className="fas fa-shopping-cart" id="cart-btn"></div>
-            <div className="fas fa-bars" id="menu-btn"></div>
+                <div className="fas fa-search" id="search-btn"></div>
+                <div className="fas fa-shopping-cart" id="cart-btn"></div>
+                <div className="fas fa-bars" id="menu-btn"></div>
+            </div>
+
+            {/*HTML for search bar*/}
+            <div className="search-form">
+                <label form="search-box" className="fas fa-search"></label>
+                <input type="search" id="search-box" placeholder="Search.."/>
+            </div>
+
+            {/*Item images and related information*/}
+            <div className="cart-items-container">
+                <div className="cart-item">
+                    {/*Adds the "x" on the cart*/}
+                    <span className="fas fa-times"></span>
+                    {/*Adds images to the cart items*/}
+                    <img src="../images/pumpkinspice.jpg" alt="Pumpkin Spice"/>
+                    <div className="content">
+                        <h3>cart item 01</h3>
+                        <div className="price">$4.50/-</div>
+                    </div>
+                </div>
+
+                <div className="cart-item">
+                    <span className="fas fa-times"></span>
+                    <img src="../images/icedcoffee.jpg" alt="Iced Coffee"/>
+                    <div className="content">
+                        <h3>cart item 02</h3>
+                        <div className="price">$4.50/-</div>
+                    </div>
+                </div>
+
+                <div className="cart-item">
+                    <span className="fas fa-times"></span>
+                    <img src="../images/waffles.jpg" alt="Smiling Waffles"/>
+                    <div className="content">
+                        <h3>cart item 03</h3>
+                        <div className="price">$4.00/-</div>
+                    </div>
+                </div>
+
+                <div className="cart-item">
+                    <span className="fas fa-times"></span>
+                    <img src="../images/fruitwaffle.jpg" alt="Fruit Waffles"/>
+                    <div className="content">
+                        <h3>cart item 04</h3>
+                        <div className="price">$4.00/-</div>
+                    </div>
+                </div>
             </div>
         </div>
-
     );
 };
 
